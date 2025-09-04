0a46730d9e31e9854481d9b36318857dae913550
Eduardo Martínez Catalá <3d24rd0@gmail.com>
2025-09-05 00:37:22 +0200

    Update pubsub_message_test.dart

--- test/models/pubsub_message_test.dart
+++ test/models/pubsub_message_test.dart
@@ -3,33 +3,31 @@ import 'package:dart_valkey/src/models/pubsub_message.dart';
 
 void main() {
   group('PubSubMessage', () {
-    group('Constructor', () {
-      test('should create a message for type "message"', () {
-        final msg = PubSubMessage(
-          type: 'message',
-          channel: 'channel1',
-          message: 'hello',
-        );
-        expect(msg.type, 'message');
-        expect(msg.channel, 'channel1');
-        expect(msg.message, 'hello');
-        expect(msg.pattern, isNull);
-        expect(msg.count, isNull);
-      });
+    test('Constructor"', () {
+      final msg = PubSubMessage(
+        type: 'pmessage',
+        pattern: 'patt*',
+        channel: 'channel2',
+        message: 'world',
+      );
+      expect(msg.type, 'pmessage');
+      expect(msg.pattern, 'patt*');
+      expect(msg.channel, 'channel2');
+      expect(msg.message, 'world');
+      expect(msg.count, isNull);
+    });
 
-      test('should create a message for type "pmessage"', () {
-        final msg = PubSubMessage(
-          type: 'pmessage',
-          pattern: 'patt*',
-          channel: 'channel2',
-          message: 'world',
-        );
-        expect(msg.type, 'pmessage');
-        expect(msg.pattern, 'patt*');
-        expect(msg.channel, 'channel2');
-        expect(msg.message, 'world');
-        expect(msg.count, isNull);
-      });
+    test('toString', () {
+      final msg = PubSubMessage(
+        type: 'pmessage',
+        pattern: 'patt*',
+        channel: 'channel2',
+        message: 'world',
+      );
+      expect(
+        msg.toString(),
+        'PubSubMessage(type: pmessage, channel: channel2, pattern: patt*, message: world)',
+      );
     });
   });
 }
